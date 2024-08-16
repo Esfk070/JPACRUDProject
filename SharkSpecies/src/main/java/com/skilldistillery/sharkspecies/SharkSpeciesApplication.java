1c7dad2fc63e32ec3208f8456327925a96038b5d
Kevin <esfk070@gmail.com>
2024-08-16 16:44:19 -0600

    Create Boot MVC project, connect JPA project dependency

---
+++ SharkSpecies/src/main/java/com/skilldistillery/sharkspecies/SharkSpeciesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sharkspecies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharkSpeciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharkSpeciesApplication.class, args);
+	}
+
+}
---
+++ SharkSpecies/src/test/java/com/skilldistillery/sharkspecies/SharkSpeciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.sharkspecies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharkSpeciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
